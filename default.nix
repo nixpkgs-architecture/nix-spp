876712f2380ec45c8616a0dc4635e44a36c76894
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-01-13 22:23:21 +0100

    Clean up Nix a bit

--- default.nix
+++ default.nix
@@ -1,25 +1,17 @@
-let
-  nixpkgs = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/cf63ade6f74bbc9d2a017290f1b2e33e8fbfa70a.tar.gz";
-    sha256 = "1aw4avc6mp3v1gwjlax6yn8984c92y56s4h7qrygxagpchkwq06j";
-  };
-
-  pkgs = import nixpkgs {
-    overlays = [];
-    config = {};
-  };
-
-  result = pkgs.rustPlatform.buildRustPackage {
-    name = "nix-refs";
-    src = pkgs.lib.cleanSource ./.;
-    cargoLock.lockFile = ./Cargo.lock;
-  };
-in result // {
-  shell = pkgs.mkShell {
-    nativeBuildInputs = [
+{
+  pkgs ? import (import ./nix/sources.nix).nixpkgs { config = {}; overlays = []; },
+}:
+pkgs.rustPlatform.buildRustPackage {
+  name = "nix-spp";
+  src = pkgs.lib.cleanSource ./.;
+  cargoLock.lockFile = ./Cargo.lock;
+  passthru.pkgs = pkgs;
+  passthru.shell = pkgs.mkShell {
+    packages = [
       pkgs.cargo
       pkgs.rust-analyzer
       pkgs.rustc
+      pkgs.niv
     ];
   };
 }
